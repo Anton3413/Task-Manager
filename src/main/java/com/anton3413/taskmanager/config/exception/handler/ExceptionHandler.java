b854ac4fa61c2e5f625689a39264552b8bdb27f7
Anton3413 <bondar8524567913@gmail.com>
2025-12-22 23:13:42 +0100

    ExceptionHandler.java

---
+++ src/main/java/com/anton3413/taskmanager/config/exception/handler/ExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.anton3413.taskmanager.config.exception.handler;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+}
