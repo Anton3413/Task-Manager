9bb392a64576ae25ffd233d83a807e27203c51e7
Anton3413 <bondar8524567913@gmail.com>
2025-12-23 10:39:51 +0100

    GlobalExceptionHandler.class added

--- src/main/java/com/anton3413/taskmanager/config/exception/handler/ExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.anton3413.taskmanager.config.exception.handler;
-
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class ExceptionHandler {
-}
