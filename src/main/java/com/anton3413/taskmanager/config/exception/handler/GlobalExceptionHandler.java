f837d03f4fde28b940f36811e51e1285aaf9edca
Anton3413 <bondar8524567913@gmail.com>
2025-12-28 17:44:02 +0100

    refactor: implement a unified error handling system
    - Renamed 404-error.html to common-error.html for universal application.
    - Upgraded GlobalExceptionHandler with dynamic status codes and custom messages.
    - Added specific handling for MethodArgumentTypeMismatchException (400 Bad Request).

--- src/main/java/com/anton3413/taskmanager/config/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/anton3413/taskmanager/config/exception/handler/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,8 +15,24 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleEntityNotFoundExc(EntityNotFoundException exception, Model model){
+        model.addAttribute("statusCode",HttpStatus.NOT_FOUND.value());
+        model.addAttribute("errorTitle",HttpStatus.NOT_FOUND.getReasonPhrase());
         model.addAttribute("errorMessage", exception.getMessage());
 
-        return "errors/404-error";
+        return "errors/common-error";
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleArgumentParsingExc(MethodArgumentTypeMismatchException exception, Model model){
+
+        final String PARAMETER_PARSING_EXCEPTION = "The provided ID must be a number. " +
+                "Please check the URL and try again.";
+
+        model.addAttribute("statusCode",HttpStatus.BAD_REQUEST.value());
+        model.addAttribute("errorTitle",HttpStatus.BAD_REQUEST.getReasonPhrase());
+        model.addAttribute("errorMessage", PARAMETER_PARSING_EXCEPTION);
+
+        return  "errors/common-error";
     }
 }
