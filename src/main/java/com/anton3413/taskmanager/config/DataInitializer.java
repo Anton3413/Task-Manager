1cafb3d66ef96910aa5382c6f5807b3954d133c2
Anton3413 <bondar8524567913@gmail.com>
2026-01-05 22:15:36 +0100

    DataInitializer little changes - unused field deleted - comments cleaned up

--- src/main/java/com/anton3413/taskmanager/config/DataInitializer.java
+++ src/main/java/com/anton3413/taskmanager/config/DataInitializer.java
@@ -11,72 +11,6 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-/*@Component
-@RequiredArgsConstructor
-public class DataInitializer implements CommandLineRunner {
-
-    private final TaskService taskService;
-    private final UserService userService;
-
-    private final String EXAMPLE_TASK_TITLE = "Demo Task";
-    private final String EXAMPLE_TASK_DESCRIPTION = """
-    This is a demonstration task. It is automatically initialized \
-    when the application starts. \
-    You can interact with it like any other task, including deleting it. :)
-    """;
-    private final String EXAMPLE_USER_USERNAME = "user";
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        if(!userService.existsByUsername(EXAMPLE_USER_USERNAME)){
-            insertExampleUser();
-        }
-        *//*if(!taskService.existsByTitleIgnoreCase(EXAMPLE_TASK_TITLE)){
-             insertExampleTask();
-        }*//*
-    }
-    private void insertExampleUser(){
-        User demoUser = User.builder()
-                .username(EXAMPLE_USER_USERNAME)
-                .password("{noop}123")
-                .email("email@example.com")
-                .createdAt(LocalDateTime.now())
-                .build();
-
-        userService.save(demoUser);
-
-        Task demoTask = Task.builder()
-                .title(EXAMPLE_TASK_TITLE)
-                .description(EXAMPLE_TASK_DESCRIPTION)
-                .createdAt(LocalDateTime.now())
-                .status(Status.NEW)
-                .dueDate(LocalDateTime.now().plusMonths(6))
-                .build();
-        taskService.save(demoTask,EXAMPLE_USER_USERNAME);
-    }
-
-    private void insertExampleUser(){
-        User demoUser = User.builder()
-                .username(EXAMPLE_USER_USERNAME)
-                .password("{noop}123")
-                .email("email@example.com")
-                .createdAt(LocalDateTime.now())
-                .build();
-
-        userService.save(demoUser);
-
-        Task demoTask = Task.builder()
-                .title(EXAMPLE_TASK_TITLE)
-                .description(EXAMPLE_TASK_DESCRIPTION)
-                .createdAt(LocalDateTime.now())
-                .status(Status.NEW)
-                .dueDate(LocalDateTime.now().plusMonths(6))
-                .build();
-        taskService.save(demoTask,EXAMPLE_USER_USERNAME);
-    }
-}*/
-
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
@@ -88,7 +22,6 @@ public class DataInitializer implements CommandLineRunner {
     when the application starts. \
     You can interact with it like any other task, including deleting it. :)
     """;
-    private final String EXAMPLE_USER_USERNAME = "user";
 
     @Override
     public void run(String... args) {
