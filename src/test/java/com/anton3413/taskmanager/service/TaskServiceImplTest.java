cb260738f3ac3be42b12504dcae37ee44f7451a9
Anton3413 <bondar8524567913@gmail.com>
2026-01-11 21:27:30 +0100

    .

---
+++ src/test/java/com/anton3413/taskmanager/service/TaskServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.anton3413.taskmanager.service;
+
+import com.anton3413.taskmanager.repository.TaskRepository;
+import com.anton3413.taskmanager.service.impl.TaskServiceImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceImplTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private SecurityService securityService;
+
+    @InjectMocks
+    TaskServiceImpl taskService;
+}
