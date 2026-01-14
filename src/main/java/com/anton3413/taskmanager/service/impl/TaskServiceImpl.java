eb91f5df63e454a9c23792931c602168ec5082e6
Anton3413 <bondar8524567913@gmail.com>
2026-01-14 21:39:39 +0100

    .

--- src/main/java/com/anton3413/taskmanager/service/impl/TaskServiceImpl.java
+++ src/main/java/com/anton3413/taskmanager/service/impl/TaskServiceImpl.java
@@ -21,6 +21,7 @@ import java.util.Optional;
 @Transactional
 public class TaskServiceImpl  implements TaskService {
 
+
     private final TaskRepository taskRepository;
     private final UserService userService;
     private final SecurityService securityService;
